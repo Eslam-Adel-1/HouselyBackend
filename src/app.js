1ab5fef668eb6cafde78469c9b19b745835bebbf
Eslam Adel <esad1263@outlook.com>
2026-04-09 17:50:11 +0200

    feat(api): implement register route and controller

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 import express from "express";
+import routes from "./routes/index.js";
 
 const app = express();
 
 // Middlewares
 app.use(express.json());
+
+// Routes
+app.use("/api", routes);
 
 // Basic health check
 app.get("/", (req, res) => {
