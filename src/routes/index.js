076f445fe6c980f159b8e338820f7cb241ca609a
Eslam Adel <esad1263@outlook.com>
2026-04-23 12:14:06 +0200

    feat(properties): add protected property routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
-import express from "express";
 import authRoutes from "./authRoutes.js";
+import propertyRoutes from "./propertyRoutes.js";
+import express from "express";
 
 //================================================
 
@@ -8,5 +9,6 @@ const router = express.Router();
 //================================================
 
 router.use("/auth", authRoutes);
+router.use("/properties", propertyRoutes);
 
 export default router;
