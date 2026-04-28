9234eef856490ab68de16839d22e497a6d5f1f09
Eslam Adel <esad1263@outlook.com>
2026-04-29 02:38:40 +0300

    feat(app): implement simple cors middleware

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 import express from "express";
 import routes from "./routes/index.js";
+import cors from "cors";
 
 const app = express();
 
 // Middlewares
 app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  }),
+);
 
 // Routes
 app.use("/api", routes);
